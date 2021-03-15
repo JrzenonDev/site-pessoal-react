354a7c55ed1c04ef63b6c111ae8638a717e3b383
jroberto <jrobertoonb@gmail.com>
2021-03-15 12:47:11 -0300

    chore: index com estrutura base do site, já utilizando o component isolado do Head

---
+++ jr-site/pages/index.js
@@ -0,0 +1,23 @@
+import Head from '../src/infra/components/Head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head title="Home - Site JrDev" />
+
+      <header>
+        Menu
+      </header>
+
+      <main>
+        Conteúdo único de cada página
+      </main>
+
+      <footer>
+        Footer
+      </footer>
+
+
+    </div>
+  );
+}
