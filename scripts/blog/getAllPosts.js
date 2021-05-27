d9d02c93da881d63e169d6a9abb44a63d10240fe
JrzenonDev <jrobertoonb@gmail.com>
2021-05-26 22:32:10 -0300

    chore: utilizando a biblioteca gray-matter para ler o arquivo markdown

--- scripts/blog/getAllPosts.js
+++ scripts/blog/getAllPosts.js
@@ -1,18 +1,20 @@
 import fs from 'fs';
-
+import grayMatter from 'gray-matter';
 
 export function getAllPosts() {
   const allPostsFilleNames = fs.readdirSync('./_posts');
 
   const posts = allPostsFilleNames.map((filename) => {
 
-    const fileContent = fs.readFileSync(`./_posts/${filename}`);
+    const fileContent = fs.readFileSync(`./_posts/${filename}`, 'utf-8');
+    const { content, data: metadata } = grayMatter(fileContent);
 
     return {
       metadata: {
+        ...metadata,
         slug: filename.replace('.md', '')
       },
-      content: fileContent.toString(),
+      content: content,
     }
 
   });
